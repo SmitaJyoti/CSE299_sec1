eb97fbdd1c170523705aa75c91aeaeeb3d75bd10
SmitaJyoti <44774736+SmitaJyoti@users.noreply.github.com>
2020-08-23 00:07:03 +0600

    Create user.java

---
+++ Code/user.java
@@ -0,0 +1,66 @@
+package com.example.identity_creator;
+
+import android.widget.RadioButton;
+
+public class users {
+    private String fullName;
+    private String username;
+    private String contactNum;
+    private String email;
+    private String password;
+    private RadioButton regA;
+    private RadioButton regC;
+
+    public users() {
+
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getContactNum() {
+        return contactNum;
+    }
+    public void setContactNum(String contactNum) {
+        this.contactNum = contactNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RadioButton getRegA() {
+        return regA;
+    }
+    public void setRegA(RadioButton regA) {
+        this.regA = regA;
+    }
+
+    public RadioButton getRegC() {
+        return regC;
+    }
+    public void setRegC(RadioButton regC) {
+        this.regC = regC;
+    }
+}
